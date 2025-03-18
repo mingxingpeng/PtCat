73b81cc1925ee2befc87f57fb654ecc4c1d9fa90
pengmingxing <2785218977@qq.com>
2025-03-18 18:11:31 +0800

    日志框架构思

---
+++ plogwriter/carve/include/pcarvebase.h
@@ -0,0 +1,21 @@
+//
+// Created by Administrator on 2025/3/18.
+//
+
+#ifndef PCARVEBASE_H
+#define PCARVEBASE_H
+
+#include "pcarve.h"
+
+namespace ptcat{
+    namespace carve{
+
+
+        class PCarveBase : public PCarve{
+
+
+        };
+    }
+}
+
+#endif //PCARVEBASE_H
