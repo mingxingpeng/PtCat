f7345d3ed4b64480243a12b0b2963634d4b2dad9
pengmingxing <2785218977@qq.com>
2025-03-18 17:06:15 +0800

    添加日志大致情况

---
+++ plogwriter/carve/include/pcarve.h
@@ -0,0 +1,10 @@
+//
+// Created by Administrator on 2025/3/12.
+//
+
+#ifndef PLOGWIRTER_H
+#define PLOGWIRTER_H
+
+
+
+#endif //PLOGWIRTER_H
---
+++ plogwriter/carve/src/pcarve.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Administrator on 2025/3/12.
+//
+
+#include "pcarve.h"
+#include <iostream>
+
+
+
+
