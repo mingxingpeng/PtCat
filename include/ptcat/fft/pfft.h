ad0764b3e4554d65fcb636ba00d23cefd48be050
pmx <2785218977@qq.com>
2026-03-25 14:06:36 +0800

    fft 目前应用没问题

--- include/ptcat/fft/pfft.h
+++ include/ptcat/fft/pfft.h
@@ -48,9 +48,10 @@ namespace ptcat{
                 }
             };
 
-            class PFFT{
+            class DLL_API PFFT{
             protected:
-                ComplexD* ft_tri_;//用于存储傅里叶变换得复数，反傅里叶变换，虚部乘以 -1
+//                ComplexD* ft_tri_;//用于存储傅里叶变换得复数，反傅里叶变换，虚部乘以 -1
+                double* ft_tri_;//用于存储傅里叶变换得复数，反傅里叶变换，虚部乘以 -1, 不再使用结构体来存储，例如 0 是实部， 1 是虚部
                 int ft_size_;//数据长度
                 Range range_;//传入进来得数据范围, 数据索引从 0 开始
                 int range_size_;
@@ -59,8 +60,6 @@ namespace ptcat{
                 double* least_squares_sum_x_;//最小二乘计算
                 int least_squares_size_;
                 int N_;//数据数量
-                ComplexD* frequency_;//生成的频域数据
-                int frequency_size_;
 
             public:
                 PFFT();
@@ -72,31 +71,33 @@ namespace ptcat{
 
                 //释放资源
                 void PFFTDeInit();
-
+            protected:
                 //创建傅里叶变换计划
-                void ExecutePFTPlan(const double* input) ;
+                void ExecutePFTPlan(const double* input, double*& frequency) ;
 
                 // SIMD 加速傅里叶变换
-                void ExecutePFTPlanAVX(const double* input) ;
+                void ExecutePFTPlanAVX(const double* input, double*& frequency) ;
 
                 //创建逆傅里叶变换计划, 做全谱的操作
-                void ExecutePIFTPlan(double*& out) ;
+                void ExecutePIFTPlan(double*& out, double*& frequency) ;
+
+                void FreeFrequency(double*& frequency);
             };
 
             class DLL_API PFFTN final : public fft::PFFT{
-
+            public:
                 //使用 SMID 加速后，速度比 未使用 SMID 的 FFTRun 快大概 100 ms
-                void PFFTRun(int N, const fft::Range& range, const double* input, double*& output, bool is_full_spectrum = false);
+                void PFFTRun(const double* input, double*& output);
 
                 //获取到振幅， 相位，幅度，这里必须全谱
-                void PFFTRun(int N, const fft::Range& range, const double* input, double*& output, double*& amplitudes,  double*& phases);
+                void PFFTRun(const Range& calc_range, const double* input, double*& output, double*& amplitudes,  double*& phases);
 
 
                 /// <summary>
                 /// 根据相位来求得数据高度，相位的斜率与包络图高度是成正比的，看相位获取的高度是否比包络图效果好
                 /// 返回斜率
                 /// </summary>
-                void PFFTPhaseRun(int N, const fft::Range& range, const double* input, double& k, double& b, bool is_full_spectrum = false);
+                void PFFTPhaseRun(const double* input, double& k, double& b);
             };
         }
     }
--- src/algorithm/fft/pfft.cpp
+++ src/algorithm/fft/pfft.cpp
@@ -41,6 +41,7 @@ namespace ptcat {
             const static double TwoPi = 2 * P_PI;
             const static double Coefficient = -(10000 / TwoPi);//通过斜率乘以该系数计算高度
 
+            static PMemoryPool& MPool = GetMP();
 
             PFFT::PFFT() : is_full_spectrum_(false),
                            range_({0, 0}),
@@ -49,9 +50,7 @@ namespace ptcat {
                            ft_size_(0),
                            least_squares_sum_x_(nullptr),
                            least_squares_size_(0),
-                           N_(0),
-                           frequency_(nullptr),
-                           frequency_size_(0)
+                           N_(0)
             {
             }
 
@@ -74,41 +73,41 @@ namespace ptcat {
 
                 //计算指定区域得傅里叶变换复数，或者全局傅里叶变换复数，也就是说是否计算全谱傅里叶变换得复数
                 range_size_ = range_.end - range_.start + 1;
-                int ft_size = range_size_ * N;
+                int N2 = N_ * 2;
+                int ft_size = range_size_ * N2;//使用 double 类型，两个 double 表示一个复数
                 //首先先判断一下是否需要修改数据获取内存
                 if (!ft_tri_ || ft_size_ != ft_size || !is_same) {//判断下数据是否一致
                     //调用内存池对数据进行获取
-                    auto &m_pool = GetMP();
 
                     if (ft_tri_)
-                        m_pool.DeAllocate(ft_tri_, ft_size_);//删除上一个数据得数据长度
+                        MPool.DeAllocate(ft_tri_, ft_size_);//删除上一个数据得数据长度
                     ft_size_ = ft_size;//然后才更新当前数据的长度，并申请内存
-                    ft_tri_ = m_pool.Allocate<ComplexD>(ft_size_);//申请内存
+                    ft_tri_ = MPool.Allocate<double>(ft_size_);//申请内存
 
-                    double N_dif = 1.0 / N;
+                    double N_dif = 1.0 / N_;
                     //进行并行运算
 #pragma omp parallel for
                     for (int i = 0; i < range_size_; i++)
                     {
                         int k = range_.start + i;
                         //计算一下数据的位置
-                        ComplexD* curr_ft_tri = &(ft_tri_[i * N]);
-                        for (int n = 0; n < N; n++)
+                        double* curr_ft_tri = &(ft_tri_[i * N2]);
+                        for (int n = 0, index = 0; n < N_; n++, index += 2)
                         {
                             double param = TwoPi * k * N_dif * n;
                             double cos_res = std::cos(param);
                             double sin_res = -std::sin(param);
-                            curr_ft_tri[n].real = cos_res;
-                            curr_ft_tri[n].imag = sin_res;
+                            curr_ft_tri[index] = cos_res;
+                            curr_ft_tri[index + 1] = sin_res;
                         }
                     }
 
                     //least_squares_sum_x_
                     if (!least_squares_sum_x_ || range_size_ != least_squares_size_){
                         if (least_squares_sum_x_)
-                            m_pool.DeAllocate(least_squares_sum_x_, least_squares_size_);
+                            MPool.DeAllocate(least_squares_sum_x_, least_squares_size_);
                         least_squares_size_ = range_size_;
-                        least_squares_sum_x_ = m_pool.Allocate<double>(least_squares_size_);
+                        least_squares_sum_x_ = MPool.Allocate<double>(least_squares_size_);
                     }
                     //得到平均值
                     double X = 0;
@@ -133,76 +132,72 @@ namespace ptcat {
                     {
                         least_squares_sum_x_[xi] = (xi - X) * dif_sum_residual;
                     }
-
-                    if (!frequency_ || range_size_ != frequency_size_){
-                        if (frequency_)
-                            m_pool.DeAllocate(frequency_, frequency_size_);
-                        frequency_size_ = range_size_;
-                        frequency_ = m_pool.Allocate<ComplexD>(frequency_size_);
-                    }
                 }
             }
 
             //释放资源
             void PFFT::PFFTDeInit() {
-                auto &m_pool = GetMP();
                 if (ft_tri_)
                 {
-                    m_pool.DeAllocate(ft_tri_, ft_size_);
+                    MPool.DeAllocate(ft_tri_, ft_size_);
                     ft_tri_ = nullptr;
                 }
                 if (least_squares_sum_x_){
-                    m_pool.DeAllocate(least_squares_sum_x_, least_squares_size_);
+                    MPool.DeAllocate(least_squares_sum_x_, least_squares_size_);
                     least_squares_sum_x_ = nullptr;
-                }
-                if (frequency_){
-                    m_pool.DeAllocate(frequency_, frequency_size_);
-                    frequency_ = nullptr;
                 }
             }
 
             //创建傅里叶变换计划
-            void PFFT::ExecutePFTPlan(const double* input) {
+            void PFFT::ExecutePFTPlan(const double* input, double*& frequency) {
+                int fre_size = range_size_ * 2;
+                frequency = MPool.Allocate<double>(fre_size);
                 //计算出频域数据，只计算范围内的频域数据,其他都为 0，对于接下来的计算没有意义
-#pragma omp parallel for//实行批量计算
-                for (int i = 0; i < range_size_; i++)
+//#pragma omp parallel for//实行批量计算
+                int N2 = N_ * 2;
+                for (int i = 0, fre_i = 0; i < range_size_; i++, fre_i += 2)
                 {
                     double sum_real = 0;
                     double sum_imag = 0;
-                    ComplexD* curr_ft_tri = &(ft_tri_[i * N_]);
-                    for (int n = 0; n < N_; n++)
+                    double* curr_ft_tri = &(ft_tri_[i * N2]);
+                    for (int n = 0, index = 0; n < N_; n++, index += 2)
                     {
                         double i_value = input[n];
-                        sum_real += curr_ft_tri[n].real * i_value;
-                        sum_imag += curr_ft_tri[n].imag * i_value;
+                        sum_real += curr_ft_tri[index] * i_value;
+                        sum_imag += curr_ft_tri[index + 1] * i_value;
                     }
-                    frequency_[i].real = sum_real;
-                    frequency_[i].imag = sum_imag;
+                    frequency[fre_i] = sum_real;
+                    frequency[fre_i + 1] = sum_imag;
                 }
             }
 
             // SIMD 加速傅里叶变换
-            void PFFT::ExecutePFTPlanAVX(const double* input) {
+            void PFFT::ExecutePFTPlanAVX(const double* input, double*& frequency) {
+                int fre_size = range_size_ * 2;
+                frequency = MPool.Allocate<double>(fre_size);
                 const int step = 4; // 每次处理 4 个 double（256 bit）
-#pragma omp parallel for//实行批量计算
-                for (int i = 0; i < range_size_; ++i) {
+                const int step2 = 2 * step; // 每次处理 4 个 double（256 bit）
+//#pragma omp parallel for//实行批量计算
+                int N2 = N_ * 2;
+                for (int i = 0, fre_i = 0; i < range_size_; ++i, fre_i += 2) {
                     __m256d sum_real = _mm256_setzero_pd();
                     __m256d sum_imag = _mm256_setzero_pd();
 
-                    ComplexD* curr_ft_tri = &(ft_tri_[i * N_]);
+                    double* curr_ft_tri = &(ft_tri_[i * N2]);
                     int n = 0;
-                    for (; n <= N_ - step; n += step) {
+                    int index = 0;
+                    for (; n <= N_ - step; n += step, index += step2) {
                         __m256d in_vec = _mm256_loadu_pd(input + n);
                         __m256d tri_real = _mm256_set_pd(
-                                curr_ft_tri[n + 3].real,
-                                curr_ft_tri[n + 2].real,
-                                curr_ft_tri[n + 1].real,
-                                curr_ft_tri[n + 0].real);
+                                curr_ft_tri[index + 6],
+                                curr_ft_tri[index + 4],
+                                curr_ft_tri[index + 2],
+                                curr_ft_tri[index + 0]);
                         __m256d tri_imag = _mm256_set_pd(
-                                curr_ft_tri[n + 3].imag,
-                                curr_ft_tri[n + 2].imag,
-                                curr_ft_tri[n + 1].imag,
-                                curr_ft_tri[n + 0].imag);
+                                curr_ft_tri[index + 7],
+                                curr_ft_tri[index + 5],
+                                curr_ft_tri[index + 3],
+                                curr_ft_tri[index + 1]);
 
                         sum_real = _mm256_fmadd_pd(in_vec, tri_real, sum_real);
                         sum_imag = _mm256_fmadd_pd(in_vec, tri_imag, sum_imag);
@@ -216,39 +211,40 @@ namespace ptcat {
                     double total_imag = imag_sum[0] + imag_sum[1] + imag_sum[2] + imag_sum[3];
 
                     // 处理剩余项
-                    for (; n < N_; ++n) {
+                    for (; n < N_; ++n, index += 2) {
                         double val = input[n];
-                        total_real += curr_ft_tri[n].real * val;
-                        total_imag += curr_ft_tri[n].imag * val;
+                        total_real += curr_ft_tri[index] * val;
+                        total_imag += curr_ft_tri[index + 1] * val;
                     }
 
-                    frequency_[i].real = total_real;
-                    frequency_[i].imag = total_imag;
+                    frequency[fre_i] = total_real;
+                    frequency[fre_i + 1] = total_imag;
                 }
             }
 
             //创建逆傅里叶变换计划, 做全谱的操作
-            void PFFT::ExecutePIFTPlan(double*& out) {
+            void PFFT::ExecutePIFTPlan(double*& out, double*& frequency) {
                 //这里计算的是幅度（包络），实部振幅，虚部表示相位，对它俩取模得到结果，获取到幅度信息
                 //可以使用 abs 进行取模，也可以使用下面的方式进行取模
                 const double N_inv = 1.0 / N_;
                 //进行 ifft
                 //因为 frequency 其他范围内的值都为 0，没有参与计算的必要
                 //ComplexD* result = new ComplexD[N];//数据不需要传出去，这里可以删除，节省 new 和 delete 时间
-#pragma omp parallel for//实行批量计算
-                for (int n = 0; n < N_; n++)
+//#pragma omp parallel for//实行批量计算
+                int N2 = 2 * N_;
+                for (int n = 0, index = 0; n < N_; n++, index += 2)
                 {
                     double sum_real = 0;
                     double sum_imag = 0;
-                    for (int i = 0; i < range_size_; i++)
+                    for (int i = 0, fre_i = 0; i < range_size_; i++, fre_i += 2)
                     {
                         //这里 ift 操作时虚部需要乘以 -1， 要取负，因为ift 计算出来是 + 虚部， ft_tri_ 是 - 虚部
                         //复数相乘,将虚部和实部先使用变量获取，避免频繁调用函数消磨时间
-                        int index = i * N_ + n;
-                        const double& ft_real = ft_tri_[index].real;
-                        const double& ft_imag = ft_tri_[index].imag;
-                        const double& fre_real = frequency_[i].real;
-                        const double& fre_imag = frequency_[i].imag;
+                        int ft_index = i * N2 + index;
+                        const double& ft_real = ft_tri_[ft_index];
+                        const double& ft_imag = ft_tri_[ft_index + 1];
+                        const double& fre_real = frequency[fre_i];
+                        const double& fre_imag = frequency[fre_i + 1];
                         sum_real += (ft_real * fre_real + ft_imag * fre_imag);
                         sum_imag += (ft_real * fre_imag - ft_imag * fre_real);
                     }
@@ -256,27 +252,37 @@ namespace ptcat {
                 }
             }
 
+            void PFFT::FreeFrequency(double*& frequency){
+                if (frequency){
+                    MPool.DeAllocate(frequency, range_size_ * 2);
+                }
+            }
+
 
 
             //使用 SMID 加速后，速度比 未使用 SMID 的 FFTRun 快大概 100 ms
-            void PFFTN::PFFTRun(int N, const fft::Range& range, const double* input, double*& output, bool is_full_spectrum) {
-                PFFTInit(N, range, is_full_spectrum);//初始化 fft, 非全谱计算
-                ExecutePFTPlanAVX(input);
-                ExecutePIFTPlan(output);
+            void PFFTN::PFFTRun(const double* input, double*& output) {
+                double* frequency = nullptr;
+                ExecutePFTPlanAVX(input, frequency);
+                ExecutePIFTPlan(output, frequency);
+                FreeFrequency(frequency);
             }
 
-            //获取到振幅， 相位，幅度，这里必须全谱
-            void PFFTN::PFFTRun(int N, const fft::Range& range, const double* input, double*& output, double*& amplitudes,  double*& phases) {
-                PFFTInit(N, range, true);//初始化 fft,全谱计算，并获取到振幅等
-                ExecutePFTPlanAVX(input);//获取到全谱频域
+            //相位和振幅通常需要全谱信息
+            //幅度可能只需要特定频段的信息, 传输入特定谱段的信息
+            void PFFTN::PFFTRun(const Range& calc_range, const double* input, double*& output, double*& amplitudes,  double*& phases) {
+                double* frequency = nullptr;
+                ExecutePFTPlanAVX(input, frequency);//获取到全谱频域
 
                 //计算出对应的数据
-                const double N_inv = 1.0 / N;
-#pragma omp parallel for
-                for (int n = 0; n < N; n++)
+                const double N_inv = 1.0 / N_;
+                int range_size = calc_range.end - calc_range.start + 1;//获取到需要计算的频域范围
+                int N2 = 2 * N_;
+//#pragma omp parallel for
+                for (int n = 0, fre_i = 0; n < N_; n++, fre_i += 2)
                 {
-                    const double& out_imag = frequency_[n].imag;
-                    const double& out_real = frequency_[n].real;
+                    const double& out_imag = frequency[fre_i + 1];
+                    const double& out_real = frequency[fre_i];
                     //计算相位
                     // 计算相位（使用atan2处理四象限）
                     double phase = atan2(out_imag, out_real);
@@ -284,23 +290,26 @@ namespace ptcat {
                     //计算振幅
                     double amp = std::sqrt(out_real * out_real + out_imag * out_imag) * N_inv;// 除以 N 进行归一化得到实际值
                     amplitudes[n] = amp;
-                    //计算幅度
+                    //计算幅度， 幅度使用计算范围 calc_range
                     double sum_real = 0;
                     double sum_imag = 0;
-                    for (int i = 0; i < range_size_; i++)
+                    for (int i = 0; i < range_size; i++)
                     {
-                        int ft_index = i * N_ + n;
+                        int calc_index = calc_range.start + i;
+                        int ft_index = calc_index * N2 + fre_i;
+                        int fre_index = calc_index * 2;
                         //这里 ift 操作时虚部需要乘以 -1， 要取负，因为ift 计算出来是 + 虚部， ft_tri_ 是 - 虚部
                         //复数相乘,将虚部和实部先使用变量获取，避免频繁调用函数消磨时间
-                        const double& ft_real = ft_tri_[ft_index].real;
-                        const double& ft_imag = ft_tri_[ft_index].imag;
-                        const double& fre_real = frequency_[i].real;
-                        const double& fre_imag = frequency_[i].imag;
+                        const double& ft_real = ft_tri_[ft_index];
+                        const double& ft_imag = ft_tri_[ft_index + 1];
+                        const double& fre_real = frequency[fre_index];
+                        const double& fre_imag = frequency[fre_index + 1];
                         sum_real += (ft_real * fre_real + ft_imag * fre_imag);
                         sum_imag += (ft_real * fre_imag - ft_imag * fre_real);
                     }
                     output[n] = std::sqrt(sum_real * sum_real + sum_imag * sum_imag) * N_inv;
                 }
+                FreeFrequency(frequency);
             }
 
 
@@ -308,20 +317,19 @@ namespace ptcat {
             /// 根据相位来求得数据高度，相位的斜率与包络图高度是成正比的，看相位获取的高度是否比包络图效果好
             /// 返回斜率
             /// </summary>
-            void PFFTN::PFFTPhaseRun(int N, const fft::Range& range, const double* input, double& k, double& b, bool is_full_spectrum) {
-                PFFTInit(N, range, is_full_spectrum);//初始化 fft
+            void PFFTN::PFFTPhaseRun(const double* input, double& k, double& b) {
+                double* frequency = nullptr;
                 //计算出频域数据，只计算范围内的频域数据,其他都为 0，对于接下来的计算没有意义，所以直接只计算范围内的频域数据，免得后续手动置零了
-                ExecutePFTPlanAVX(input);
+                ExecutePFTPlanAVX(input, frequency);
                 //根据这段范围内的频域数据，因为我只取对应范围内的数据， 获取到相位数据，
-                auto& m_pool = GetMP();
-                double* phases = m_pool.Allocate<double>(range_size_);//存储振幅数据
+                double* phases = MPool.Allocate<double>(range_size_);//存储振幅数据
                 //振幅计算公式
                 int n = 0;
                 double last_phase = 0;
-                for (int i = 0; i < range_size_; i++)
+                for (int i = 0, fre_i = 0; i < range_size_; i++, fre_i += 2)
                 {
                     // 计算相位（使用atan2处理四象限）, -- pmx 这里使用 三角函数，可能会有性能消耗，后续优化使用 SIMD
-                    phases[i] = std::atan2(frequency_[i].imag, frequency_[i].real); // [-π, π]
+                    phases[i] = std::atan2(frequency[fre_i + 1], frequency[fre_i]); // [-π, π]
                     //将其绘制成为还原回一条直线
                     if (i != 0)//首位不需要设置
                         if (phases[i] > last_phase) n++;
@@ -355,8 +363,8 @@ namespace ptcat {
 
                 //释放资源
                 if (phases)
-                    m_pool.DeAllocate(phases, range_size_);
-
+                    MPool.DeAllocate(phases, range_size_);
+                FreeFrequency(frequency);
             }
         }
     }
